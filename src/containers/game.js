dbdc972e71c9666706433e9bce27fa2b2bcce3af
Rachel <rachel@Rachels-MBP-2.flashtalking.lds>
2016-03-22 17:59:46 +0000

    basic routes setup

---
+++ src/containers/game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+
+export default class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Let's Play </h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './routes';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,7 +12,9 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <div>
+          <Router history={browserHistory} routes={routes} />
+        </div>
+    </Provider>
   , document.querySelector('.container'));
